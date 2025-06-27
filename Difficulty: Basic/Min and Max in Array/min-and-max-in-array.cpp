fb14c38927021ffb0df0530f6c486915a0a7ecc7
Abhijay shah <115077886+Abhijayshah@users.noreply.github.com>
2025-06-27 15:56:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Min and Max in Array/min-and-max-in-array.cpp
+++ Difficulty: Basic/Min and Max in Array/min-and-max-in-array.cpp
@@ -2,11 +2,21 @@
 class Solution {
   public:
     pair<int, int> getMinMax(vector<int> arr) {
-        // code here
-        int n = arr.size();
-        sort(arr.begin() , arr.end());
-        pair<int,int> p = make_pair(arr[0] , arr[n-1]);
-        return p;
+        // Initialize min_val and max_val to the first element of the array
+        int min_val = arr[0], max_val = arr[0];
         
+        // Traverse through the array
+        for (int i = 1; i < arr.size(); i++) {
+            // Update min and max values
+            if (arr[i] < min_val) {
+                min_val = arr[i];
+            }
+            if (arr[i] > max_val) {
+                max_val = arr[i];
+            }
+        }
+        
+        // Return the pair of min and max values
+        return {min_val, max_val};
     }
 };
