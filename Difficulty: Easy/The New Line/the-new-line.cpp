41daa6a79e3b1481ce01267a2b7e79e2456ce289
Abhijay shah <115077886+Abhijayshah@users.noreply.github.com>
2025-08-25 23:54:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/The New Line/the-new-line.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+// Function to print each word in new line
+void printNewLine() {
+    // Your code here
+    cout << "Geeks" << endl;
+    cout << "for" << endl;
+    cout << "Geeks" << endl;
+}
