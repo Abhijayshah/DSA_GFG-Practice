2f3d54cc1928947ec36da7ab36eff4b449b08fc1
Abhijay Kumar Shah <115077886+Abhijayshah@users.noreply.github.com>
2025-12-12 18:51:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Reverse a String/reverse-a-string.c
+++ Difficulty: Basic/Reverse a String/reverse-a-string.c
@@ -1,16 +1,18 @@
-#include <string.h>
+#include <string.h>   // For strlen()
 
 char* reverseString(char* s) {
-    int left = 0;
-    int right = strlen(s) - 1;
+    int left = 0;                   // Pointer starting from the beginning
+    int right = strlen(s) - 1;      // Pointer starting from the end
 
-    while (left < right) {
-        char temp = s[left];
-        s[left] = s[right];
-        s[right] = temp;
-        left++;
-        right--;
+    while (left < right) {          // Swap characters until pointers meet
+
+        char temp = s[left];        // Store left character temporarily
+        s[left] = s[right];         // Copy right character to the left position
+        s[right] = temp;            // Put temp (original left char) into right position
+
+        left++;                     // Move left pointer forward
+        right--;                    // Move right pointer backward
     }
 
-    return s;
+    return s;                       // Return the reversed string
 }
