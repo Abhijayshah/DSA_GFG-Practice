96a34d00a054272204e02d7162388907b8ce1e96
Abhijay Kumar Shah <115077886+Abhijayshah@users.noreply.github.com>
2026-01-06 23:32:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Segregate 0s and 1s/segregate-0s-and-1s.cpp
+++ Difficulty: Easy/Segregate 0s and 1s/segregate-0s-and-1s.cpp
@@ -1,20 +1,27 @@
+// User function template for C++
+
 class Solution {
   public:
     void segregate0and1(vector<int> &arr) {
-        int left = 0, right = arr.size() - 1;
-
-        while (left < right) {
-            // Move left pointer until we find 1
-            while (arr[left] == 0 && left < right) left++;
-            // Move right pointer until we find 0
-            while (arr[right] == 1 && left < right) right--;
-            
-            // Swap 1 on left side with 0 on right side
-            if (left < right) {
-                swap(arr[left], arr[right]);
+        int left = 0 , right = arr.size() -1;
+        
+        while(left <right){
+            while(left<right && arr[left ]==0){
                 left++;
-                right--;
+                
             }
+     
+        
+        while(left<right && arr[right]==1){
+            right--;
         }
+        
+        if(left <right){
+            swap(arr[left],arr[right]);
+            left++;
+            right--;
+            
+        }
+      }
     }
 };
