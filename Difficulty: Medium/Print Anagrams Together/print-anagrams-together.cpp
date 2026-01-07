a170615a49e3df5c173485e985ded4d2d11f91a3
Abhijay Kumar Shah <115077886+Abhijayshah@users.noreply.github.com>
2026-01-07 22:24:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Print Anagrams Together/print-anagrams-together.cpp
@@ -0,0 +1,26 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<vector<string>> anagrams(vector<string>& arr) {
+        // code here
+        unordered_map<string, vector<string>> mp;
+        
+        for(string word : arr){
+            string key = word;
+            sort(key.begin(), key.end());
+            
+            mp[key].push_back(word);
+            
+        }
+        vector<vector<string>> result;
+        
+        for (auto &it : mp){
+            result.push_back(it.second);
+            
+        }
+        
+        return result;
+        
+    }
+};
