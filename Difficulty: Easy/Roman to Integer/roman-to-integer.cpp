5976328560e8a6143c90897fa503d351bdcef13c
Abhijay Kumar Shah <115077886+Abhijayshah@users.noreply.github.com>
2026-01-30 23:25:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Roman to Integer/roman-to-integer.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    int romanToDecimal(string &s) {
+        // code here
+        unordered_map<char,int> mp = {
+            {'I',1},
+            {'V',5},
+            {'X',10},
+            {'L',50},
+            {'C',100},
+            {'D',500},
+            {'M',1000},
+        };
+        int ans = 0;
+        if(s.size()==1)
+        return mp[s[0]];
+        for(int i=0;i<s.size();i++){
+            if(mp[s[i]]>=mp[s[i+1]])
+            ans =  ans + mp[s[i]];
+            else if
+            (mp[s[i]]<mp[s[i+1]])
+            ans = ans - mp[s[i]];
+            }
+            return ans;
+    }
+};
